d46e3f3e71d8b89e1bafca46e163aa21812e8c53
Kishankharvi <kishankharvi702@gmail.com>
2025-08-02 20:40:19 +0530

    hgfj

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // filepath: frontend/src/utils/api.js
 
-const API_BASE_URL = import.meta.env.REACT_APP_API_URL ;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 console.log("API Base URL:", API_BASE_URL);
 class ApiService {
   constructor() {
