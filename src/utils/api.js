3c9ff9aadba03ea27ea1d9be4d499916014c58a2
Kishankharvi <kishankharvi702@gmail.com>
2025-08-02 21:48:09 +0530

    jdjhf

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
-// filepath: frontend/src/utils/api.js import.meta.env.VITE_REACT_APP_API_URL|| 
+// filepath: frontend/src/utils/api.js 
 
-const API_BASE_URL = "https://nexus-store-d2d4c8bb6f52.herokuapp.com/api" ;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL|| "https://nexus-store-d2d4c8bb6f52.herokuapp.com/api" ;
 console.log("API Base URL:", API_BASE_URL);
 
 if (!API_BASE_URL) {
