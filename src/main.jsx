a72f1edf904e900d38ab3cd5aecb67074130b6ae
Kishankharvi <kishankharvi702@gmail.com>
2025-08-03 11:08:53 +0530

    added more PWA featuresdd

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,16 @@
-// frontend/src/main.jsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
-
-// Function to ask for notification permissions
-function askForNotificationPermission() {
-  Notification.requestPermission().then(result => {
-    if (result === 'granted') {
-      console.log('Notification permission granted.');
-      // You would typically send the subscription object to your server here
-    }
-  });
-}
+)
 
 // This part registers the service worker
 if ("serviceWorker" in navigator) {
@@ -27,8 +19,6 @@ if ("serviceWorker" in navigator) {
       .register("/sw.js")
       .then((registration) => {
         console.log("Service Worker registered: ", registration);
-        // Ask for notification permission after the service worker is ready
-        setTimeout(askForNotificationPermission, 5000); // Delay for a better user experience
       })
       .catch((registrationError) => {
         console.log("Service Worker registration failed: ", registrationError);
